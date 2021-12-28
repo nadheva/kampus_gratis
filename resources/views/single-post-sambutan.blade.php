2b08f6831287c71084c88054b97fc6df6f4a18cd
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2021-12-28 13:47:52 +0700

    push

---
+++ resources/views/single-post-sambutan.blade.php
@@ -0,0 +1,50 @@
+<x-app-layout>
+    <section class="pt-5 pb-0">
+        <div class="container">
+            <div class="row g-0 g-lg-5">
+    
+                <!-- Left sidebar START -->
+                <div class="col-lg-4">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-12">
+                            <!-- Instructor image START -->
+                <div class="card shadow p-2 mb-4 text-center">
+                  <div class="rounded-3">
+                    <!-- Image -->
+                    <img src="assets/images/instructor/07.jpg" class="card-img" alt="course image">
+                  </div>
+        
+                  <!-- Card body -->
+                  <div class="card-body px-3">
+                    <ul class="list-inline mb-0">
+                      <li class="list-inline-item"> <a class="btn px-2 btn-sm bg-facebook" href="#"><i class="fab fa-fw fa-facebook-f"></i></a> </li>
+                      <li class="list-inline-item"> <a class="btn px-2 btn-sm bg-instagram-gradient" href="#"><i class="fab fa-fw fa-instagram"></i></a> </li>
+                      <li class="list-inline-item"> <a class="btn px-2 btn-sm bg-twitter" href="#"><i class="fab fa-fw fa-twitter"></i></a> </li>
+                      <li class="list-inline-item"> <a class="btn px-2 btn-sm bg-linkedin" href="#"><i class="fab fa-fw fa-linkedin-in"></i></a> </li>
+                    </ul>
+                  </div>
+                </div>
+                </div>
+                <div class="col-md-6 col-lg-12">
+        
+                </div>
+                </div> <!-- Row End -->
+                </div>
+                <!-- Left sidebar END -->
+                    
+                <!-- Main content START -->
+                <div class="col-lg-8">
+                
+                    <!-- Title -->
+                    <h5 class="mb-0">Hi, I am</h5>
+                    <h1 class="mb-0">Lori Stevens</h1>
+                    <p>Graphic Designer</p>
+                    <!-- Content -->
+                    <p class="mt-4">Satisfied conveying a dependent contented he gentleman agreeable do be. Warrant private blushes removed an in equally totally if. Delivered dejection necessary objection do Mr prevailed. Mr feeling does chiefly cordial in do.</p>
+                    <p>We focus a great deal on the understanding of behavioral psychology and influence triggers which are crucial for becoming a well-rounded Digital Marketer. We understand that theory is important to build a solid foundation, we understand that theory alone isn’t going to get the job done so that’s why this course is packed with practical hands-on examples that you can follow step by step.</p>
+                </div>
+    
+            </div>
+        </div>
+    </section>
+</x-app-layout>
