3a9d84758f7c47d6cf7b7697dbd4101ca0531e29
erzaffasya <58721335+erzaffasya@users.noreply.github.com>
2022-01-04 18:49:45 +0700

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,183 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
+<head>
+    <title>Eduport - LMS, Education and Course Theme</title>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+    <!-- Meta Tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="author" content="Webestica.com">
+    <meta name="description" content="Eduport- LMS, Education and Course Theme">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;700&family=Roboto:wght@400;500;700&display=swap">
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+    <!-- Plugins CSS -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/font-awesome/css/all.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css') }}">
+
+    <!-- Theme CSS -->
+    <link id="style-switch" rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+
+<body>
+
+    <!-- **************** MAIN CONTENT START **************** -->
+    <main>
+        <section class="p-0 d-flex align-items-center position-relative overflow-hidden">
+
+            <div class="container-fluid">
+                <div class="row">
+                    <!-- left -->
+                    <div
+                        class="col-12 col-lg-6 d-md-flex align-items-center justify-content-center bg-primary bg-opacity-10 vh-lg-100">
+                        <div class="p-3 p-lg-5">
+                            <!-- Title -->
+                            <div class="text-center">
+                                <h2 class="fw-bold">Welcome to our largest community</h2>
+                                <p class="mb-0 h6 fw-light">Let's learn something new today!</p>
+                            </div>
+                            <!-- SVG Image -->
+                            <img src="assets/images/element/02.svg" class="mt-5" alt="">
+                            <!-- Info -->
+                            <div class="d-sm-flex mt-5 align-items-center justify-content-center">
+                                <!-- Avatar group -->
+                                <ul class="avatar-group mb-2 mb-sm-0">
+                                    <li class="avatar avatar-sm">
+                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/01.jpg"
+                                            alt="avatar">
+                                    </li>
+                                    <li class="avatar avatar-sm">
+                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/02.jpg"
+                                            alt="avatar">
+                                    </li>
+                                    <li class="avatar avatar-sm">
+                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/03.jpg"
+                                            alt="avatar">
+                                    </li>
+                                    <li class="avatar avatar-sm">
+                                        <img class="avatar-img rounded-circle" src="assets/images/avatar/04.jpg"
+                                            alt="avatar">
+                                    </li>
+                                </ul>
+                                <!-- Content -->
+                                <p class="mb-0 h6 fw-light ms-0 ms-sm-3">4k+ Students joined us, now it's your turn.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Right -->
+                    <div class="col-12 col-lg-6 m-auto">
+                        <div class="row my-5">
+                            <div class="col-sm-10 col-xl-8 m-auto">
+                                <!-- Title -->
+                                <span class="mb-0 fs-1">👋</span>
+                                <h1 class="fs-2">Login into Eduport!</h1>
+                                <p class="lead mb-4">Nice to see you! Please log in with your account.</p>
+
+                                <!-- Form START -->
+                                <form method="post" action="{{ route('login') }}">
+                                    @csrf
+                                    <!-- Email -->
+                                    <div class="mb-4">
+                                        <label for="exampleInputEmail1" class="form-label">Email address *</label>
+                                        <div class="input-group input-group-lg">
+                                            <span
+                                                class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i
+                                                    class="bi bi-envelope-fill"></i></span>
+                                            <input type="email" name="email" class="form-control border-0 bg-light rounded-end ps-1"
+                                                placeholder="E-mail" id="exampleInputEmail1">
+                                        </div>
+                                    </div>
+                                    <!-- Password -->
+                                    <div class="mb-4">
+                                        <label for="inputPassword5" class="form-label">Password *</label>
+                                        <div class="input-group input-group-lg">
+                                            <span
+                                                class="input-group-text bg-light rounded-start border-0 text-secondary px-3"><i
+                                                    class="fas fa-lock"></i></span>
+                                            <input name="password" type="password"
+                                                class="form-control border-0 bg-light rounded-end ps-1"
+                                                placeholder="password" id="inputPassword5">
+                                        </div>
+                                        <div id="passwordHelpBlock" class="form-text">
+                                            Your password must be 8 characters at least
+                                        </div>
+                                    </div>
+                                    <!-- Check box -->
+                                    <div class="mb-4 d-flex justify-content-between mb-4">
+                                        <div class="form-check">
+                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                            <label class="form-check-label" for="exampleCheck1">Remember me</label>
+                                        </div>
+                                        <div class="text-primary-hover">
+                                            <a href="forgot-password.html" class="text-secondary">
+                                                <u>Forgot password?</u>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <!-- Button -->
+                                    <div class="align-items-center mt-0">
+                                        <div class="d-grid">
+                                            <button type="submit" class="btn btn-primary mb-0" type="button">Login</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                <!-- Form END -->
+
+                                <!-- Social buttons and divider -->
+                                <div class="row">
+                                    <!-- Divider with text -->
+                                    <div class="position-relative my-4">
+                                        <hr>
+                                        <p
+                                            class="small position-absolute top-50 start-50 translate-middle bg-body px-5">
+                                            Or</p>
+                                    </div>
+
+                                    <!-- Social btn -->
+                                    <div class="col-xxl-6 d-grid">
+                                        <a href="#" class="btn bg-google mb-2 mb-xxl-0"><i
+                                                class="fab fa-fw fa-google text-white me-2"></i>Login with Google</a>
+                                    </div>
+                                    <!-- Social btn -->
+                                    <div class="col-xxl-6 d-grid">
+                                        <a href="#" class="btn bg-facebook mb-0"><i
+                                                class="fab fa-fw fa-facebook-f me-2"></i>Login with Facebook</a>
+                                    </div>
+                                </div>
+
+                                <!-- Sign up link -->
+                                <div class="mt-4 text-center">
+                                    <span>Don't have an account? <a href="sign-up.html">Signup here</a></span>
+                                </div>
+                            </div>
+                        </div> <!-- Row END -->
+                    </div>
+                </div> <!-- Row END -->
             </div>
+        </section>
+    </main>
+    <!-- **************** MAIN CONTENT END **************** -->
 
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+    <!-- Back to top -->
+    <div class="back-top"><i class="bi bi-arrow-up-short position-absolute top-50 start-50 translate-middle"></i>
+    </div>
 
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
 
-            <!-- Remember Me -->
-            <div class="block mt-4">
-                <label for="remember_me" class="inline-flex items-center">
-                    <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+    <!-- Template Functions -->
+    <script src="{{ asset('assets/js/functions.js') }}"></script>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+</body>
 
-                <x-button class="ml-3">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -65,4 +65,5 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
 require __DIR__.'/auth.php';
