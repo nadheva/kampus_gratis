3309626d66d5800549aa9e947f2fe613239f3c65
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-01-07 06:42:05 +0700

    push

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -4,6 +4,10 @@
 <!-- Vendors -->
 <script src="{{asset('assets/vendor/purecounterjs/dist/purecounter_vanilla.js')}}"></script>
 <script src="{{asset('assets/vendor/apexcharts/js/apexcharts.min.js')}}"></script>
+ <!-- Js conf delete-->
+ <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js"></script>
+ <script type="text/javascript">
 
 <!-- Template Functions -->
 <script src="{{asset('assets/js/functions.js')}}"></script>
