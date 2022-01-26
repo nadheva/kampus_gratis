6bc0a5f5dc64173bc1c3894fd543b1f58de0e9f0
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-01-26 14:55:02 +0700

    push

--- resources/views/landingpage/partials/scripts.blade.php
+++ resources/views/landingpage/partials/scripts.blade.php
@@ -17,5 +17,17 @@
 
 
 <x-Alert/>
-<script src="https://cdn.tiny.cloud/1/pbt8hv1pilzu9hy3yh33hvz7bnbdtjda34vxywwge56tcjou/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61f0c9ce9bd1f31184d95e10/1fqa98k89';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 @stack('scripts')
