050eb970a654ab963ddc452d8fb1cebd6105bd4a
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-02-16 21:31:53 +0700

    Revert "w"
    This reverts commit a8e030f70d8275c945de12d0e1609186bf480ad7.

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -140,7 +140,7 @@ class BerandaController extends Controller
         // dd($request);
         $userid = Auth::user()->id;
         // $request = $client->post('http://dashboard.kampusgratis.id/api/administrasi',
-        $request = $client->put('http://127.0.0.1:8080/api/administrasi/'.$userid,
+        $request = $client->put('http://127.0.0.1:8000/api/administrasi',
         ['form_params' =>
         [
             'user_id' => $userid,
