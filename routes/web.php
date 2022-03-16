939fdf16575b238120afcb936b0ffd53f7366eb6
EGYuda23 <egyyudanugraha@gmail.com>
2022-03-16 11:01:07 +0700

    Tambah route testing pada web.php

--- routes/web.php
+++ routes/web.php
@@ -119,6 +119,12 @@ Route::middleware(['auth', 'role:mahasiswa'])->group(function () {
 Route::middleware(['auth', 'role:admin,mahasiswa'])->group(function () {
 });
 
+Route::get('/testing_byyuda', function(){
+    return response()->json([
+        'msg'=>'Testing by Yuda'
+    ]);
+});
+
 // Route Login
 
 require __DIR__ . '/auth.php';
