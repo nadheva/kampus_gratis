49b188c75aee3890663cf51c99e32eed0a640a0a
fauzanstya <100351337+fauzanstya@users.noreply.github.com>
2022-03-16 11:21:23 +0700

    Ubah route di web.php

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,11 @@ use App\Http\Controllers\UserDashboardController;
 |
 */
 
+Route::get('/test-ojan', function() {
+    return response()->json([
+        "pesan"=>"Test123"
+    ]);
+});
 
 Route::get('/tentang', [BerandaController::class, 'tentang'])->name('tentang');
 Route::get('/mahasiswa', [BerandaController::class, 'mahasiswa'])->name('mahasiswa');
@@ -118,6 +123,8 @@ Route::middleware(['auth', 'role:mahasiswa'])->group(function () {
 
 Route::middleware(['auth', 'role:admin,mahasiswa'])->group(function () {
 });
+
+
 
 // Route Login
 
