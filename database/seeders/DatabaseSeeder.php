5c0187f029cbdd5aafe51e387beb33e143068a1e
Nadheva Derdika <59642869+nadheva@users.noreply.github.com>
2022-03-23 09:12:54 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             QuestionSeeder::class,
             BeritaSeeder::class,
-            FaqSeeder::class
+            FaqSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
